696521b72b61dcf3c707c57dee646d182c2922f1
Matheus Hygino <matheusghygino@gmail.com>
2024-12-16 01:30:26 -0300

    adicionado TodoList no app.js

--- todo-list/src/App.js
+++ todo-list/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
 import TodoItem from './components/TodoItem'
+import TodoList from './components/TodoList';
 
 function App() {
-  const todo = { id:1, text: 'Aprender react', completed: false };
+  const todos = [
+    { id:1, text: 'Aprender react', completed: false },
+    {id:2, tex: 'Fazer uma lista de tarefas', completed: true}
+  ]
 
   const handleDelete = (id) => {
     console.log("Excluir tarefa com ID: ", id)
@@ -15,7 +19,7 @@ function App() {
   return (
     <div className="text-center mt-10">
       <h1 className="text-4xl font-bold text-blue-600 mb-6">Minha lista de Tarefas</h1>
-    <TodoItem todo={todo} onDelete={handleDelete} onToggle={handleToggle} />
+    <TodoList todos={todos} onDelete={handleDelete} onToggle={handleToggle} />
     </div>
   );
 }
