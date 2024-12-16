ee2f654fe76b246da9223c41c52da7483ae45fee
Matheus Hygino <matheusghygino@gmail.com>
2024-12-16 01:23:20 -0300

    TodoItem

--- todo-list/src/components/TodoItem.js
+++ todo-list/src/components/TodoItem.js
@@ -1,16 +1,30 @@
 import React from "react";
 
-const todoItem = ( { todo, onDelete, onToggle } ) => {
-    return (
-        <div className="flex items-center justify-between p-4 border-b border-gray-200">
-            <input 
-                type="checkbox"
-                checked={todo.completed}
-                onChange={() => onToggle(todo.id)}
-            />
-            <span></span>
-        </div>
-    )
+const todoItem = ({ todo, onDelete, onToggle }) => {
+  return (
+    <div className="flex items-center justify-between p-4 border-b border-gray-200">
+      <div className="flex items-center">
+        <input
+          type="checkbox"
+          checked={todo.completed}
+          onChange={() => onToggle(todo.id)}
+        />
+        <span
+          className={`text-lg ${
+            todo.completed ? "line-through text-gray-400" : "text-gray-800"
+          }`}
+        >
+          {todo.text}
+        </span>
+      </div>
+      <button
+        onClick={() => onDelete(todo.id)}
+        className="text-red-500 hover:text-red-700"
+      >
+        Excluir
+      </button>
+    </div>
+  );
 };
 
 export default todoItem;
