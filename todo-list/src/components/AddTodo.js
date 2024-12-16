75e401c058f148d06973c30ccc71a13615743b4b
Matheus Hygino <matheusghygino@gmail.com>
2024-12-16 13:56:41 -0300

    a

--- todo-list/src/components/AddTodo.js
+++ todo-list/src/components/AddTodo.js
@@ -23,7 +23,7 @@ const AddTodo = ({ onAdd }) => {
                 type="submit"
                 className="px-4 py-2 bg-blue-600 text-white rounded-r-lg hover:bg-blue-700"
             >
-                Adicionar
+                Adicionar 
             </button>
         </form>
     )
